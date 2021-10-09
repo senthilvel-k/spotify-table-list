25532ac582bfaa9f15545c8be526bf7b3aa660aa
senthilvelk31 <86486178+senthilvelk31@users.noreply.github.com>
2021-10-09 23:18:58 +0530

    Add files via upload

---
+++ tabular.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from tabulate import tabulate
+
+df = pd.read_csv(open("GFG.csv", encoding="utf8"))
+
+df2 = df[df.genre == 'metal']
+df3 = df[df.genre == 'blues']
+df4 = df[df.genre == 'dance']
+df5 = df[df.genre == 'jazz']
+frames = [df2, df3, df4, df5]
+result = pd.concat(frames)
+req = result[['track', 'artist', 'spotify_id']]
+final = req.sample(n=20)
+print(tabulate(final, headers = 'keys', tablefmt = 'psql'))
